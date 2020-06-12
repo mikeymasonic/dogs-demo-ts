b49af4c8068bd281a6a6927bf958d0230156212d
Michael Romay <amerror@gmail.com>
2020-06-12 13:23:59 -0700

    created fetch call for all breeds

--- src/services/dogsAPI.ts
+++ src/services/dogsAPI.ts
@@ -1,1 +1,6 @@
-export {}
+export const fetchList = () => {
+  return fetch('https://dog.ceo/api/breeds/list/all')
+    .then(res => res.json())
+    .then(json => json.message)
+    .then(json => Object.keys(json));
+};
