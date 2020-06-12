350c62b6dda6c4c870fb3d32e68d331df878b72e
Michael Romay <amerror@gmail.com>
2020-06-12 13:20:06 -0700

    created List component

--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -1,1 +1,15 @@
-export {}
+import React from 'react';
+
+const List = ({ breed }: { breed: string }) => (
+  <section>
+    <input
+      type="radio"
+      value={breed}
+      name="dogs"
+      id={breed}
+      />
+    <label htmlFor={breed}>{breed}</label>
+  </section>
+);
+
+export default List;
