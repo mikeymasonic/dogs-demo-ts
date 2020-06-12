a916ecd1d19a9ed29f8530cfa7b964ad53ef9a94
Michael Romay <amerror@gmail.com>
2020-06-12 16:01:48 -0700

    made provider happy

--- src/hooks/breedProvider.tsx
+++ src/hooks/breedProvider.tsx
@@ -1,27 +1,29 @@
-import React, { createContext, useState, useContext, useEffect } from 'react';
-import { fetchList, fetchDetail } from '../services/dogsAPI';
+import React, { createContext, useState, useContext, useEffect } from "react";
+import { fetchList, fetchDetail } from "../services/dogsAPI";
 
-export interface BreedContextInterface {
-  breeds: Array<string>,
-  filteredBreeds: Array<string>,
-  searchText: string,
-  breedDetail: Array<string>,
-  loading: boolean
-}
+export type BreedContext = {
+  breeds: Array<string>;
+  filteredBreeds: Array<string>;
+  searchText: string;
+  breedDetail: Array<string>;
+  loading: boolean;
+  handleChange: (a: { target: any }) => void;
+  handleDetail: (breed: string) => void;
+};
 
-const BreedContext = createContext<BreedContextInterface | null>(null);
+const BreedContext = createContext<BreedContext | null>(null);
 
 export const BreedProvider = ({ children }: { children: React.ReactNode }) => {
-  const [breeds, setBreeds] = useState([]);
-  const [filteredBreeds, setFilteredBreeds] = useState([]);
-  const [searchText, setSearchText] = useState('');
-  const [breedDetail, setBreedDetail] = useState([]);
+  const [breeds, setBreeds] = useState<string[]>([]);
+  const [filteredBreeds, setFilteredBreeds] = useState<string[]>([]);
+  const [searchText, setSearchText] = useState("");
+  const [breedDetail, setBreedDetail] = useState<string[]>([]);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     setLoading(true);
     fetchList()
-      .then(apiBreeds => setBreeds(apiBreeds))
+      .then((apiBreeds) => setBreeds(apiBreeds))
       .finally(() => setLoading(false));
   }, []);
 
@@ -30,34 +32,66 @@ export const BreedProvider = ({ children }: { children: React.ReactNode }) => {
   }, [breeds]);
 
   useEffect(() => {
-    const searchResults = breeds.filter(breed => breed.includes(searchText));
+    const searchResults = breeds.filter((breed) => breed.includes(searchText));
     setLoading(true);
     setFilteredBreeds(searchResults.slice(0, 12));
     setLoading(false);
   }, [breeds, searchText]);
 
-  const handleChange = ({ target }) => {
+  const handleChange = ({ target }: { target: any }) => {
     setSearchText(target.value);
   };
 
-  const handleDetail = (breeds) => {
+  const handleDetail = (breed: string) => {
     setLoading(true);
-    fetchDetail(breeds)
-      .then(apiBreeds => setBreedDetail(apiBreeds))
+    fetchDetail(breed)
+      .then((apiBreeds) => setBreedDetail(apiBreeds))
       .finally(() => setLoading(false));
   };
 
-  
+  return (
+    <BreedContext.Provider
+      value={{
+        breeds,
+        filteredBreeds,
+        searchText,
+        breedDetail,
+        handleChange,
+        handleDetail,
+        loading,
+      }}
+    >
+      {children}
+    </BreedContext.Provider>
+  );
+};
 
+export const useBreeds = () => {
+  return useContext(BreedContext)?.breeds;
+};
 
-  
-  
+export const useFilteredBreeds = () => {
+  return useContext(BreedContext)?.filteredBreeds;
+};
 
+export const useSearchText = () => {
+  return useContext(BreedContext)?.searchText;
+};
 
+export const useBreedDetail = () => {
+  return useContext(BreedContext)?.breedDetail;
+};
 
+export const useHandleChange = () => {
+  return useContext(BreedContext)?.handleChange;
+};
 
+export const useHandleDetail = () => {
+  return useContext(BreedContext)?.handleDetail;
+};
 
-}
+export const useLoading = () => {
+  return useContext(BreedContext)?.loading;
+};
 
-
-
+export default BreedContext;
--- src/services/dogsAPI.ts
+++ src/services/dogsAPI.ts
@@ -1,12 +1,12 @@
 export const fetchList = () => {
-  return fetch('https://dog.ceo/api/breeds/list/all')
-    .then(res => res.json())
-    .then(json => json.message)
-    .then(json => Object.keys(json));
+  return fetch("https://dog.ceo/api/breeds/list/all")
+    .then((res) => res.json())
+    .then((json) => json.message)
+    .then((json) => Object.keys(json));
 };
 
 export const fetchDetail = (breedName: string) => {
   return fetch(`https://dog.ceo/api/breed/${breedName}/images`)
-    .then(res => res.json())
-    .then(json => (json.message));
+    .then((res) => res.json())
+    .then((json) => json.message);
 };
