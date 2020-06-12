79b80d5956f065165eea572d293fa012494bceaf
Michael Romay <amerror@gmail.com>
2020-06-12 13:26:57 -0700

    created fetch call for images by breedName

--- src/services/dogsAPI.ts
+++ src/services/dogsAPI.ts
@@ -4,3 +4,9 @@ export const fetchList = () => {
     .then(json => json.message)
     .then(json => Object.keys(json));
 };
+
+export const fetchDetail = (breedName: string) => {
+  return fetch(`https://dog.ceo/api/breed/${breedName}/images`)
+    .then(res => res.json())
+    .then(json => (json.message));
+};
