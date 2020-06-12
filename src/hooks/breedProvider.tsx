0fa221cf83298937777871191df3178c302b6ee5
Michael Romay <amerror@gmail.com>
2020-06-12 13:48:43 -0700

    setup state and inital context provider - created useEffect to pull in fetch call data

--- src/hooks/breedProvider.tsx
+++ src/hooks/breedProvider.tsx
@@ -1,1 +1,38 @@
-export {}
+import React, { createContext, useState, useContext, useEffect } from 'react';
+import { fetchList } from '../services/dogsAPI';
+
+export interface BreedContextInterface {
+  breeds: Array<string>,
+  filteredBreeds: Array<string>,
+  searchText: string,
+  breedDetail: Array<string>,
+  loading: boolean
+}
+
+const BreedContext = createContext<BreedContextInterface | null>(null);
+
+export const BreedProvider = ({ children }: { children: React.ReactNode }) => {
+  const [breeds, setBreeds] = useState([]);
+  const [filteredBreeds, setFilteredBreeds] = useState([]);
+  const [searchText, setSearchText] = useState('');
+  const [breedDetail, setBreedDetail] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(true);
+    fetchList()
+      .then(apiBreeds => setBreeds(apiBreeds))
+      .finally(() => setLoading(false));
+  }, []);
+
+  
+  
+
+
+
+
+
+}
+
+
+
