54068eb4f495029baa17d5609472f881b88fdfd7
Michael Romay <amerror@gmail.com>
2020-06-12 17:08:01 -0700

    wrote BreedDetail tsx

--- src/container/BreedDetail/BreedDetail.tsx
+++ src/container/BreedDetail/BreedDetail.tsx
@@ -1,1 +1,22 @@
-export {}
+import React from 'react';
+import { useBreedDetail, useLoading } from '../../hooks/breedProvider';
+import Details from '../../components/Details/Details';
+
+const BreedDetail = () => {
+  const breed = useBreedDetail();
+  const loading = useLoading();
+  const breedDetailNodes = breed?.map((breeds) => {
+    return <Details key={breeds} breed={breeds} />;
+  });
+
+  if (loading)
+    return (
+      <section>
+        <h2>loading...</h2>
+      </section>
+    );
+
+  return <section>{breedDetailNodes}</section>;
+};
+
+export default BreedDetail;
